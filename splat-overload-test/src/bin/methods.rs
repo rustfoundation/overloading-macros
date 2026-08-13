7c22080418514a090f6dd10dca50b26589373daf
teor <teor@riseup.net>
2026-08-13 14:19:50 +1000

    Make clippy happy again
    Co-authored-by: teor <teor@riseup.net>

--- splat-overload-test/src/bin/methods.rs
+++ splat-overload-test/src/bin/methods.rs
@@ -1,7 +1,7 @@
 #![feature(splat)]
 #![feature(tuple_trait)]
 #![allow(incomplete_features)]
-#![allow(unused_braces)]
+#![allow(unused_braces, clippy::disallowed-names)]
 
 use splat_overload::overload;
 
@@ -17,5 +17,5 @@ overload! {
 fn main() {
     let foo = Foo;
     foo.method(42i32);
-    foo.method(3.14f64);
+    foo.method(3.1f64);
 }
