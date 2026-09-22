50efcf1dba82152ed101244e4c57d76df00ce74c
teor <teor@riseup.net>
2026-09-22 13:32:01 +0200

    Add FIXME notes to the build script

--- cpp-overload-test/build.rs
+++ cpp-overload-test/build.rs
@@ -1,5 +1,6 @@
-extern crate cpp_build;
 fn main() {
     cpp_build::build("src/bin/std-forward-list-ok.rs");
-    cpp_build::build("src/bin/std-forward-list-fail.rs");
+    // FIXME: move these into the crate root instead of examples, `cpp_build` doesn't work with
+    // multiple examples.
+    //cpp_build::build("src/bin/std-forward-list-fail.rs");
 }
