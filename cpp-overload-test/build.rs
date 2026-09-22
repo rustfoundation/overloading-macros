aca84ebbb1e778e16765b61c4853fb486e5541dd
teor <teor@riseup.net>
2026-09-22 20:23:22 +0200

    Ignore a C++ deprecation in the cpp crate

--- cpp-overload-test/build.rs
+++ cpp-overload-test/build.rs
@@ -4,5 +4,7 @@ fn main() {
     cpp_build::Config::new()
         // Required for range overloads.
         .flag("-std=c++23")
+        // Ignore `warning: 'std::rel_ops' is deprecated: use '<=>' instead [-Wdeprecated-declarations]`
+        .flag("-Wno-deprecated-declarations")
         .build("src/lib.rs");
 }
