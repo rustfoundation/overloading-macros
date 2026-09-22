4540fd4dafa2772775ee1a51d492f932363be847
teor <teor@riseup.net>
2026-09-22 17:54:27 +0200

    Add iterator and range overloads

--- cpp-overload-test/build.rs
+++ cpp-overload-test/build.rs
@@ -1,5 +1,8 @@
 fn main() {
     // `cpp_build` doesn't work with multiple example/binary builds in the same crate, so we use
     // lib.rs submodules for successful overloads.
-    cpp_build::build("src/lib.rs");
+    cpp_build::Config::new()
+        // Required for range overloads.
+        .flag("-std=c++23")
+        .build("src/lib.rs");
 }
