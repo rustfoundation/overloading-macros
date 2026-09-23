37fcffe2a67396b65e6bba3ebc8617331446b896
teor <teor@riseup.net>
2026-09-23 10:18:46 +0200

    Fake generics, use c_size_t, improve overload coverage

--- cpp-overload-test/src/lib.rs
+++ cpp-overload-test/src/lib.rs
@@ -1,7 +1,7 @@
 //! Test C++ overloads that can be successfully represented in Rust.
 //! Each type in the C++ standard library has a module under `stdcpp`.
 
-#![feature(splat, tuple_trait)]
+#![feature(splat, tuple_trait, c_size_t)]
 #![allow(incomplete_features)]
 
 /// The C++ standard library.
